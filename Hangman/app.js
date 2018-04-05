692cf8446aee108a68093f422e9405b778340be6
Elliot Yandzio <elliotyandzio@gmail.com>
2018-04-05 12:31:29 +0100

    working with 2 stick men

--- Hangman/app.js
+++ Hangman/app.js
@@ -7,12 +7,19 @@ $(function() {
   const randomWord = library[Math.floor(Math.random()*library.length)];
   console.log('chosenWord-->', randomWord);
   let found = false;
-  const $showHide = $('.showHide');
   let head = false;
   let body = false;
   let arms = false;
   let left = false;
   let right = false;
+  let head1 = false;
+  let body1 = false;
+  let arms1 = false;
+  let left1 = false;
+  let right1 = false;
+  let player2turn = false;
+  let stand = false;
+  let stand1 = false;
 
 
   //creates amount of divs for the length of the word taken from the array
@@ -23,12 +30,6 @@ $(function() {
   for (let k = 0; k<alphabet.length; k++){
     $('.keyboard').append('<div class="keys">' + alphabet[k] + '</div>');
   }
-
-  $showHide.click(function() {
-    $('#head, #body, #arms, #left, #right').css('opacity', '1');
-  });
-
-
 
 
   $(document).keypress(function (e) {
@@ -45,26 +46,68 @@ $(function() {
           found = true;
         }
       } if(!found) {
-        console.log('This letter isnt in the word');
-        if(head === false) {
-          $('#head').css('opacity', '1');
-          head = true;
-          console.log(body);
-        } else if (body === false && head === true) {
-          $('#body').css('opacity', '1');
-          body = true;
-          console.log(body);
-        } else if(arms === false && head === true && body === true) {
-          $('#arms').css('opacity', '1');
-          arms = true;
-        } else if (left === false && head === true && body === true && arms === true){
-          $('#left').css('opacity', '1');
-          left = true;
-        } else if(right === false && head === true && body === true && arms === true && left === true) {
-          $('#right').css('opacity', '1');
-          right = true;
+        if(player2turn === false) {
+          console.log('This letter isnt in the word');
+          if(stand === false) {
+            $('.top, .rope, .stand, .floor').css('opacity', '1');
+            stand = true;
+            player2turn = true;
+          } else if(stand === true && head === false) {
+            $('.head').css('opacity', '1');
+            head = true;
+            player2turn = true;
+            console.log(body);
+          } else if (stand === true && body === false && head === true) {
+            $('.torso').css('opacity', '1');
+            body = true;
+            player2turn = true;
+            console.log(body);
+          } else if(stand === true && arms === false && head === true && body === true) {
+            $('.arms').css('opacity', '1');
+            arms = true;
+            player2turn = true;
+          } else if (stand === true && left === false && head === true && body === true && arms === true){
+            $('.left').css('opacity', '1');
+            left = true;
+            player2turn = true;
+          } else if(stand === true && right === false && head === true && body === true && arms === true && left === true) {
+            $('.right').css('opacity', '1');
+            right = true;
+            player2turn = true;
+          } else {
+            alert('You have run out of life .... GAME OVER - Player Two Wins!');
+          }
         } else {
-          alert('You have run out of life .... GAME OVER');
+          console.log('This letter isnt in the word');
+          if(stand1 === false) {
+            $('.top1, .rope1, .stand1, .floor1').css('opacity', '1');
+            stand1 = true;
+            player2turn = false;
+          } else if(stand1 === true && head1 === false) {
+            $('.head1').css('opacity', '1');
+            head1 = true;
+            player2turn = false;
+            console.log(body);
+          } else if (stand1 === true && body1 === false && head1 === true) {
+            $('.torso1').css('opacity', '1');
+            body1 = true;
+            player2turn = false;
+            console.log(body);
+          } else if(stand1 === true && arms1 === false && head1 === true && body1 === true) {
+            $('.arms1').css('opacity', '1');
+            arms1 = true;
+            player2turn = false;
+          } else if (stand1 === true &&  left1 === false && head1 === true && body1 === true && arms1 === true){
+            $('.left1').css('opacity', '1');
+            left1 = true;
+            player2turn = false;
+          } else if(stand1 === true && right1 === false && head1 === true && body1 === true && arms1 === true && left1 === true) {
+            $('.right1').css('opacity', '1');
+            right1 = true;
+            player2turn = false;
+          } else {
+            alert('You have run out of life .... GAME OVER - Player One Wins!');
+          }
         }
       }
     }
