3ba5ae532d42e54e32be831ccb9e86cfcedffe85
Elliot Yandzio <elliotyandzio@gmail.com>
2018-04-05 09:15:43 +0100

    hangman working when the wrong letters are inputted but still an error if you click on the correct letter then an incorrect one, it doesnt show the stick figure anymore

--- Hangman/app.js
+++ Hangman/app.js
@@ -1,14 +1,34 @@
 $(function() {
   const indices = [ ];
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'];
   const library  = ['banana', 'orange', 'apple', 'mango'];
   const randomWord = library[Math.floor(Math.random()*library.length)];
   console.log('chosenWord-->', randomWord);
+  let found = false;
+  const $showHide = $('.showHide');
+  let head = false;
+  let body = false;
+  let arms = false;
+  let left = false;
+  let right = false;
 
 
   //creates amount of divs for the length of the word taken from the array
-  for (var i = 0; i<randomWord.length; i++){
-    $('body').append('<div class="wordLetters"></div>');
+  for (let i = 0; i<randomWord.length; i++){
+    $('.reel').append('<div class="wordLetters"></div>');
   }
+
+  for (let k = 0; k<alphabet.length; k++){
+    $('.keyboard').append('<div class="keys">' + alphabet[k] + '</div>');
+  }
+
+  $showHide.click(function() {
+    $('#head, #body, #arms, #left, #right').css('opacity', '1');
+  });
+
+
 
 
   $(document).keypress(function (e) {
@@ -22,8 +42,32 @@ $(function() {
           //console.log('indexOf guess--->', i);
           //console.log('indice--->',indices);
           $('.wordLetters:eq('+ i +')').text(guess);
+          found = true;
+        }
+      } if(!found) {
+        console.log('This letter isnt in the word');
+        if(head === false) {
+          $('#head').css('opacity', '1');
+          head = true;
+          console.log(body);
+        } else if (body === false && head === true) {
+          $('#body').css('opacity', '1');
+          body = true;
+          console.log(body);
+        } else if(arms === false && head === true && body === true) {
+          $('#arms').css('opacity', '1');
+          arms = true;
+        } else if (left === false && head === true && body === true && arms === true){
+          $('#left').css('opacity', '1');
+          left = true;
+        } else if(right === false && head === true && body === true && arms === true && left === true) {
+          $('#right').css('opacity', '1');
+          right = true;
+        } else {
+          alert('You have run out of life .... GAME OVER');
         }
       }
     }
   });
+
 });
